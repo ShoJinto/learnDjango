5f998d88de7671eb036105ca070e4b666ab20947
肖江涛 <shojinto@mmyueche.com>
2017-11-09 11:14:44 +0800

    document format

--- check_api/check_api.py
+++ check_api/check_api.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python
-#coding:utf-8
+# coding:utf-8
 
-import urllib2 
-from urllib2 import HTTPError,URLError
+import urllib2
+from urllib2 import HTTPError, URLError
 
 import time
 
@@ -14,7 +14,7 @@ from email.mime.text import MIMEText
 from email.header import Header
 
 import logging
-from os import path,makedirs
+from os import path, makedirs
 
 import redis
 from redis.exceptions import RedisError
@@ -29,121 +29,129 @@ def redisClient(host=config.redis_host, port=config.redis_port):
     port    :redis server port
     '''
     try:
-        pool= redis.ConnectionPool(host=host,port=port)
-        r=redis.Redis(connection_pool=pool)
+        pool = redis.ConnectionPool(host=host, port=port)
+        r = redis.Redis(connection_pool=pool)
         return r
     except RedisError as err:
-        logging.error("Redis error: "+str(err))
+        logging.error("Redis error: " + str(err))
 
 
-#logger setting
-if path.exists(config.logpath) != True: makedirs(config.logpath,existk=True)
-logger=logging.basicConfig(
-    filename=config.logpath+"check_api.log",
+# logger setting
+if path.exists(config.logpath) != True:
+    makedirs(config.logpath, existk=True)
+logger = logging.basicConfig(
+    filename=config.logpath + "check_api.log",
     format="%(asctime)s -%(name)s-%(levelname)s-%(module)s:%(message)s",
     datefmt="%Y-%m-%d %H:%M:%S %p",
     level=logging.DEBUG
-    )
-    
+)
 
-def sendemail(submsg,frommsg,msg):
+
+def sendemail(submsg, frommsg, msg):
     '''
     need three parameters
     submsg:     subject string
     frommsg:    email from where
     msg:        email content
-    '''        
-    message=MIMEText(msg,"plain","utf-8") # 设置邮件内容
-    message['From']=Header(frommsg)
-    message['To']=Header("mm-ops","utf-8")
-    
-    subject=submsg
-    message['Subject']=Header(subject,'utf-8')
-    
+    '''
+    message = MIMEText(msg, "plain", "utf-8")  # 设置邮件内容
+    message['From'] = Header(frommsg)
+    message['To'] = Header("mm-ops", "utf-8")
+
+    subject = submsg
+    message['Subject'] = Header(subject, 'utf-8')
+
     try:
-        smtpObj=smtplib.SMTP()
-        smtpObj.connect(config.mail_host,25)
-        smtpObj.login(config.mail_user,config.mail_pass)
+        smtpObj = smtplib.SMTP()
+        smtpObj.connect(config.mail_host, 25)
+        smtpObj.login(config.mail_user, config.mail_pass)
         smtpObj.sendmail(config.sender, config.receivers, message.as_string())
         logging.info("send warning message successfully")
     except smtplib.SMTPException as err:
-        logging.error("send warning message failed "+str(err))
+        logging.error("send warning message failed " + str(err))
     except Exception as err:
-        logging.error("send warning message failed "+str(err))
+        logging.error("send warning message failed " + str(err))
 
-   
-def reporter(sta_info,report,msg):
+
+def reporter(sta_info, report, msg):
     '''
     this is a sendmail call function
     sta_info    station information
     report      senduser
     msg         error message
     '''
-    logging.error(sta_info+": "+msg)
-    sendemail(sta_info,report,sta_info+": "+msg+"\nat: "+time.strftime("%Y-%m-%d %H:%M:%S")) # add timestemp
-    
-def errorHandling(station,url,err):
+    logging.error(sta_info + ": " + msg)
+    sendemail(sta_info, report, sta_info + ": " + msg + "\nat: " +
+              time.strftime("%Y-%m-%d %H:%M:%S"))  # add timestemp
+
+
+def errorHandling(station, url, err):
     '''
     将经过检查有误的站点和错误次数作为reids的key和value进行存放，并设置key的超时时间。这样就达到了在规定时间内如果错误次数超过规定次数就进行报警的目的
     '''
-    #如果该站点是第一次被检测出有问题，说明redis中根本就没有该站点的任何信息，这个if not ... else 是对错误次数进行初始化
+    # 如果该站点是第一次被检测出有问题，说明redis中根本就没有该站点的任何信息，这个if not ... else 是对错误次数进行初始化
     if not redisClient().get(station):
-        er_count=0
-        redisClient(config.redis_host, config.redis_port).set(station, int(er_count)+1, ex=config.interval)
+        er_count = 0
+        redisClient(config.redis_host, config.redis_port).set(
+            station, int(er_count) + 1, ex=config.interval)
     else:
-        er_count=int(redisClient().get(station)) #由于从redis中get到的value是str类型所以需要进行数据类型转换
-        ttl=redisClient(config.redis_host, config.redis_port).ttl(station) #获取redis中问题站点错误次数过期时间，用于刷新超时时间
-        redisClient().set(station, int(er_count)+1,ex=ttl)
-    
-    if er_count >= config.count:
-        reporter("[%s]:%s" % (station,url), "check api", str(err))
-        redisClient().delete(station) #达到报警条件，发送完报警之后，需要对redis中的信息进行重置
-#         redisClient().rpush("retrystation_list",station)
-        redisClient().hset("retrystation_list", station, url) #将需要重新检测的车站信息放入redis中
-        
+        # 由于从redis中get到的value是str类型所以需要进行数据类型转换
+        er_count = int(redisClient().get(station))
+        ttl = redisClient(config.redis_host, config.redis_port).ttl(
+            station)  # 获取redis中问题站点错误次数过期时间，用于刷新超时时间
+        redisClient().set(station, int(er_count) + 1, ex=ttl)
 
-def checking(station,url,isretry=False):
+    if er_count >= config.count:
+        reporter("[%s]:%s" % (station, url), "check api", str(err))
+        redisClient().delete(station)  # 达到报警条件，发送完报警之后，需要对redis中的信息进行重置
+#         redisClient().rpush("retrystation_list",station)
+        redisClient().hset("retrystation_list", station, url)  # 将需要重新检测的车站信息放入redis中
+
+
+def checking(station, url, isretry=False):
     '''
     this is checking function
     need two parameters
     station:    is station name
     url:          is station threeparty api url
     '''
-    response_code=dict()
+    response_code = dict()
     try:
-        response=urllib2.urlopen(url)
-        response_code[url]=response.getcode()
-        if isretry :
-            reporter("[%s]:%s" % (station,url), "check api", "is recovered!")
-            redisClient().hdel("retrystation_list", station) #重新检测发现没有问题的车站将从redis移除
+        response = urllib2.urlopen(url)
+        response_code[url] = response.getcode()
+        if isretry:
+            reporter("[%s]:%s" % (station, url), "check api", "is recovered!")
+            redisClient().hdel("retrystation_list", station)  # 重新检测发现没有问题的车站将从redis移除
         else:
-            logging.info("[%s]:%s is ok!" % (station,url))
+            logging.info("[%s]:%s is ok!" % (station, url))
     except HTTPError as err:
         errorHandling(station, url, err)
     except URLError as err:
         errorHandling(station, url, err)
     except Exception as err:
         errorHandling(station, url, err)
-        
+
+
 def check_api():
     '''
     this is check api heath function
-    '''   
+    '''
     if config.useUrls == True:
-        station=config.station_name  
+        station = config.station_name
         for url in config.urls:
-            checking(station,url)
-            
+            checking(station, url)
+
         if redisClient().hlen("retrystation_list") > 0:
             for re_station, re_url in redisClient().hgetall("retrystation_list").items():
                 checking(re_station, re_url, isretry=True)
     else:
         for station, url in config.stationUrlMapping.items():
-            checking(station,url)
-            
+            checking(station, url)
+
         if redisClient().hlen("retrystation_list") > 0:
             for re_station, re_url in redisClient().hgetall("retrystation_list").items():
                 checking(re_station, re_url, isretry=True)
 
-if __name__=="__main__":
+
+if __name__ == "__main__":
     check_api()
--- check_api/config.py
+++ check_api/config.py
@@ -1,52 +1,52 @@
 #!/usr/bin/env python
-#coding:utf-8
+# coding:utf-8
 
-#urls和statationUrlMapping只能任选其一
-#default is False it is meaning use statationUrlMapping
-useUrls=False
+# urls和statationUrlMapping只能任选其一
+# default is False it is meaning use statationUrlMapping
+useUrls = False
 
-#默认站点名称
-statation_name="美美出行"
+# 默认站点名称
+statation_name = "美美出行"
 
-#api接口url对应的车站
-statationUrlMapping={
-#     "绵阳南湖":"http://2.100.100.14/czweb/StationService.asmx",
-#     "绵阳北川":"http://2.100.100.4/czweb/StationService.asmx",
-#     "绵阳富乐车站":"http://2.100.100.15/czweb/StationService.asmx",
-#     "绵阳江油":"http://2.100.100.8/czweb/StationService.asmx",
-#     "眉山洪雅":"http://2.100.100.3/czweb/StationService.asmx",
-#     "眉山中心站":"http://2.100.100.2/czweb/StationService.asmx",
-#     "遂宁城南":"http://2.100.100.11/czweb/StationService.asmx",
-#     "遂宁城北":"http://2.100.100.12/czweb/StationService.asmx",
-#     "遂宁射洪":"http://2.100.100.9/czweb/StationService.asmx",
-#     "遂宁蓬溪":"http://2.100.100.7/czweb/StationService.asmx",
-#     "遂宁安居":"http://2.100.100.13/czweb/StationService.asmx"
-"美美出行":"http://ms.mmchuxing.com/test/conn",
-"百度":"http://www.baidu11111111.com",
-"新浪":"http://www.sina.com"
-    }
+# api接口url对应的车站
+statationUrlMapping = {
+    #     "绵阳南湖":"http://2.100.100.14/czweb/StationService.asmx",
+    #     "绵阳北川":"http://2.100.100.4/czweb/StationService.asmx",
+    #     "绵阳富乐车站":"http://2.100.100.15/czweb/StationService.asmx",
+    #     "绵阳江油":"http://2.100.100.8/czweb/StationService.asmx",
+    #     "眉山洪雅":"http://2.100.100.3/czweb/StationService.asmx",
+    #     "眉山中心站":"http://2.100.100.2/czweb/StationService.asmx",
+    #     "遂宁城南":"http://2.100.100.11/czweb/StationService.asmx",
+    #     "遂宁城北":"http://2.100.100.12/czweb/StationService.asmx",
+    #     "遂宁射洪":"http://2.100.100.9/czweb/StationService.asmx",
+    #     "遂宁蓬溪":"http://2.100.100.7/czweb/StationService.asmx",
+    #     "遂宁安居":"http://2.100.100.13/czweb/StationService.asmx"
+    "美美出行": "http://ms.mmchuxing.com/test/conn",
+    "百度": "http://www.baidu11111111.com",
+    "新浪": "http://www.sina.com"
+}
 
-#需要检查的url
+# 需要检查的url
 # url="http://ms.mmchuxing.com/test/conn"
-urls=["http://ms.mmchuxing.com/test/connssss",]
+urls = ["http://ms.mmchuxing.com/test/connssss", ]
 
-#redis配置，
-redis_host="localhost"
-redis_port="6379"
+# redis配置，
+redis_host = "localhost"
+redis_port = "6379"
 
-#报警规则设置参数
-#count 出现错误的次数，interval 错误次数在这个时间段内进行累加，参考设置的意思是：如果在300毫秒内错误次数超过3此就需要报警
-count=3
-interval=300
+# 报警规则设置参数
+# count 出现错误的次数，interval 错误次数在这个时间段内进行累加，参考设置的意思是：如果在300毫秒内错误次数超过3此就需要报警
+count = 3
+interval = 300
 
-#第三方SMTP服务
-mail_host="smtp.mmyueche.com"
-mail_user="itservice@mmyueche.com"
-mail_pass="XULEI520xulei"
+# 第三方SMTP服务
+mail_host = "smtp.mmyueche.com"
+mail_user = "itservice@mmyueche.com"
+mail_pass = "XULEI520xulei"
 
-#发送邮件帐号设置
-sender="itservice@mmyueche.com"
-receivers=["shojinto@mmchuxing.com"]  
+# 发送邮件帐号设置
+sender = "itservice@mmyueche.com"
+receivers = ["shojinto@mmchuxing.com"]
 
-#log保存路径记得最后的斜杠！！
-logpath="/var/log/"
+# log保存路径记得最后的斜杠！！
+logpath = "/var/log/"
